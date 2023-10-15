4c23b988818739d261328145b2c604a701d31eb3
ashishkhanagwal20 <ashishstudy20@gmail.com>
2023-10-15 11:15:18 +0530

    unicafe exercise 1.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
+  const handlegood = () => {
+    setGood(good + 1);
+  };
+  const handleneutral = () => {
+    setNeutral(neutral + 1);
+  };
+  const handlebad = () => {
+    setBad(bad + 1);
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Give Feedback</h2>
+      <Button handleClick={handlegood} text="Good" />
+      <Button handleClick={handleneutral} text="Neutral" />
+      <Button handleClick={handlebad} text="Bad" />
+      <h2>Statistics</h2>
+      <br />
+      <p>Good : {good}</p>
+      <p>Neutral : {neutral}</p>
+      <p>Bad : {bad}</p>
     </>
-  )
-}
+  );
+};
 
-export default App
+const Button = ({ text, handleClick }) => {
+  console.log(text);
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+export default App;
